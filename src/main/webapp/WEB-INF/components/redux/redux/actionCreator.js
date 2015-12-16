3f8163dbc50facfdec2d8bc94e772e181bb143b7
Eugene Kashida <ekashida@gmail.com>
2015-12-16 09:40:03 -0800

    Implement makeActionCreator

--- src/main/webapp/WEB-INF/components/redux/redux/actionCreator.js
+++ src/main/webapp/WEB-INF/components/redux/redux/actionCreator.js
@@ -1,9 +1,19 @@
 function actionCreator (actionTypes) { // eslint-disable-line no-unused-vars
-  var creators = {
-    init: function () {
-      return { type: actionTypes.INIT };
-    }
-  };
+
+  // Can be used to make simple action creators that don't need to close over any internal variables
+  function makeActionCreator (type) {
+    var propertyNames = Array.prototype.slice.call(arguments, 1);
+    return function () {
+      var args = Array.prototype.slice.call(arguments);
+      var action = { type: type };
+      propertyNames.forEach(function (name, index) {
+        action[name] = args[index];
+      });
+      return action;
+    };
+  }
+
+  var creators = {};
 
   creators.addTodo = function (text) {
     var nextId = 0;
@@ -14,6 +24,7 @@ function actionCreator (actionTypes) { // eslint-disable-line no-unused-vars
     };
   };
 
+  // creators.destroyTodo = makeActionCreator(actionTypes.DESTROY_TODO, 'id');
   creators.destroyTodo = function (id) {
     return {
       type: actionTypes.DESTROY_TODO,
@@ -21,6 +32,14 @@ function actionCreator (actionTypes) { // eslint-disable-line no-unused-vars
     };
   };
 
+  // creators.init = makeActionCreator(actionTypes.INIT);
+  creators.init = function () {
+    return {
+      type: actionTypes.INIT
+    };
+  };
+
+  // creators.setVisibilityFilter = makeActionCreator(actionTypes.SET_VISIBILITY_FILTER, 'filter');
   creators.setVisibilityFilter = function (filter) {
     return {
       type: actionTypes.SET_VISIBILITY_FILTER,
@@ -28,6 +47,7 @@ function actionCreator (actionTypes) { // eslint-disable-line no-unused-vars
     };
   };
 
+  // creators.toggleTodo = makeActionCreator(actionTypes.TOGGLE_TODO, 'id');
   creators.toggleTodo = function (id) {
     return {
       type: actionTypes.TOGGLE_TODO,
